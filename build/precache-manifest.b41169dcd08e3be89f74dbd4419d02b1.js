14047ee234afb352fe62746b49e2746bb27a9685
Kate <katerina_ti@mail.ru>
2018-11-26 13:33:14 +0300

    cleanup

---
+++ build/precache-manifest.b41169dcd08e3be89f74dbd4419d02b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "beb20e767d6b1fc48883",
+    "url": "/listapp/static/css/main.d5af881e.chunk.css"
+  },
+  {
+    "revision": "beb20e767d6b1fc48883",
+    "url": "/listapp/static/js/main.beb20e76.chunk.js"
+  },
+  {
+    "revision": "57dd0772c45ea10d11fe",
+    "url": "/listapp/static/css/1.c33b9f5d.chunk.css"
+  },
+  {
+    "revision": "57dd0772c45ea10d11fe",
+    "url": "/listapp/static/js/1.57dd0772.chunk.js"
+  },
+  {
+    "revision": "c8febdd857fc196a1975",
+    "url": "/listapp/static/js/runtime~main.c8febdd8.js"
+  },
+  {
+    "revision": "59fe48309335595d4f9be77706ee4b56",
+    "url": "/listapp/index.html"
+  }
+];
